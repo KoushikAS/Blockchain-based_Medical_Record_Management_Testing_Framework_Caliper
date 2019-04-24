61e626908ba0220992e3d80390a78eb0ef788c66
Koushik <askoushik4@gmail.com>
2019-04-24 12:15:21 +0530

    update

--- benchmark/composer/composer-samples/healthcare1.js
+++ benchmark/composer/composer-samples/healthcare1.js
@@ -1,46 +1,61 @@
 'use strict';
 
 const removeExisting = require('../composer-test-utils').clearAll;
-const logger = require('../../../src/comm/util').getLogger('vehicle-lifecycle-network.js');
+const composerUtils = require('../../../src/adapters/composer/composer_utils');
+const logger = require('../../../src/comm/util').getLogger('healthcare.js');
 const os = require('os');
 const uuid = os.hostname() + process.pid; // UUID for client within test
 
 module.exports.info  = 'Healthcaare Network Performance Test';
 
 let bc;
-let busNetConnection;
+let busNetConnections;
 let testAssetNum;
 let testTransaction;
 let factory;
-let assetId;
+let populated;
+let patient;
 const base_ns = 'org.healthcare.basic';
+const busNetName = 'healthcare';
 
 
 module.exports.init = async function(blockchain, context, args) {
     bc = blockchain;
-    busNetConnection = context;
+    busNetConnections = new Map();
+    busNetConnections.set('admin', context);
     testAssetNum = args.testAssets;
     testTransaction = args.transaction;
 
-    assetId = 0;
-    factory = busNetConnection.getBusinessNetwork().getFactory();
+    factory = busNetConnections.get('admin').getBusinessNetwork().getFactory();
 
-    let doctorRegistry = await busNetConnection.get('admin').getParticipantRegistry(base_ns + '.Doctor');
-    let patientRegistry = await busNetConnection.get('admin').getParticipantRegistry(base_ns + '.Patient');  
-    let medicalInfoRegistry = await busNetConnection.get('admin').getAssetRegistry(base_ns + '.MedicalInfo'); 
+    let doctorRegistry = await busNetConnections.get('admin').getParticipantRegistry(base_ns + '.Doctor');
+    let patientRegistry = await busNetConnections.get('admin').getParticipantRegistry(base_ns + '.Patient');  
+    let medicalInfoRegistry = await busNetConnections.get('admin').getAssetRegistry(base_ns + '.MedicalInfo'); 
 
     let doctor= new Array();
-    let patient = new Array();
-    let medicalInfo = new Array();
+    let patients = new Array();
+    let medicalInfos = new Array();
 
     switch(testTransaction) {
-        case 'GivePermission':          
-        patient.push(factory.newResource(namespace, 'Patient', 'Patient_0'));
+        case 'GivePermission':    
+        patient = factory.newResource(base_ns, 'Patient', 'Patient_0');
+        patient.firstName = "Peter";
+        patient.lastName = "Parker";
+        patient.age = "18";
+        patient.address = "NYU";
+        patient.phNo = "8888888888";      
+        patients.push(patient);
+
         for(let i = 0; i < testAssetNum; i ++){
-            medicalInfo.push(factory.newResource(base_ns,  'MedicalInfo', 'MedicalInfo_' + i));
+            let medicalInfo = factory.newResource(base_ns,  'MedicalInfo', 'MedicalInfo_' + i);
+            medicalInfo.owner = factory.newRelationship(base_ns, 'Patient', 'Patient_0');
+            medicalInfo.medication = 'Spider Bite Medicine';
+            medicalInfo.pastVisitsArray = [];
+            medicalInfo.permissionedDoctorsId = [];
+            medicalInfos.push(medicalInfo);
         }
 
-        populated = await patientRegistry.exists(patient.getIdentifier());
+        populated = await patientRegistry.exists(patients[0].getIdentifier());
 
         break;
         default:
@@ -50,9 +65,9 @@ module.exports.init = async function(blockchain, context, args) {
     try{
         if(!populated){
             logger.info('Adding test assets ...');
-            await patientRegistry.addAll(patient);
+            await patientRegistry.addAll(patients);
             await doctorRegistry.addAll(doctor);
-            await medicalInfoRegistry.addAll(medicalInfo);
+            await medicalInfoRegistry.addAll(medicalInfos);
             logger.info('Asset addition complete ...');
         } else {
             // Second test pass, update/remove
@@ -60,11 +75,17 @@ module.exports.init = async function(blockchain, context, args) {
             await removeExisting(patientRegistry, 'Patient_');
             await removeExisting(doctorRegistry, 'Doctor_');
             await removeExisting(medicalInfoRegistry, 'MedicalInfo_');
-            await patientRegistry.addAll(patient);
+            await patientRegistry.addAll(patients);
             await doctorRegistry.addAll(doctor);
-            await medicalInfoRegistry.addAll(medicalInfo);
+            await medicalInfoRegistry.addAll(medicalInfos);
             logger.info('Asset update complete ...');
         }
+
+        logger.info('About to create new patient card');
+        let userName = 'PatientCard_0';
+        let newConnection = await composerUtils.obtainConnectionForParticipant(busNetConnections.get('admin'), busNetName, patient, userName);
+        busNetConnections.set(userName, newConnection);        
+
     } catch (error) {
         logger.error('error in test init(): ', error);
         return Promise.reject(error);
@@ -79,7 +100,7 @@ module.exports.run = function() {
         transaction.MedicalInfo = factory.newRelationship(base_ns, 'MedicalInfo', 'MedicalInfo_0' );
         transaction.doctorId = 'Doctor_0' ;  
         logger.info('GIVEPERMISSION:'+transaction)
-        return  bc.bcObj.submitTransaction(busNetConnection.get('PatientCard_' + uuid + '_0' ), transaction);   
+        return  bc.bcObj.submitTransaction(busNetConnections.get('admin' ), transaction);   
     default: 
         throw new Error('No valid test Transaction specified');
     }
